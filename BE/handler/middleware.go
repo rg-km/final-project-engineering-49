3cc54c3992e09482f2e43bfda1cea8d5a25cede3
nashihulibad <alamin.ibad@gmail.com>
2022-06-09 12:13:34 +0700

    chore: updating middleware

--- BE/handler/middleware.go
+++ BE/handler/middleware.go
@@ -9,7 +9,7 @@ import (
 func (h *Handler) CheckUser(c *gin.Context) {
 	ok := h.repo.IsLogin(c.Request.Header["Token"])
 	if !ok {
-		c.JSON(http.StatusForbidden,"Forbidden")
+		c.AbortWithStatusJSON(http.StatusForbidden,"Forbidden")
 		return
 	} else{
 		c.Next()
@@ -19,7 +19,7 @@ func (h *Handler) CheckUser(c *gin.Context) {
 func (h *Handler) CheckAdmin(c *gin.Context) {
 	ok := h.repo.IsAdmin(c.Request.Header["Token"])
 	if !ok {
-		c.JSON(http.StatusForbidden,"Forbidden")
+		c.AbortWithStatusJSON(http.StatusForbidden,"Forbidden")
 		return
 	} else{
 		c.Next()
