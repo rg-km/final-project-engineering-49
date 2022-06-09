47dbe84421c5ba79231893a8da497f5837b4aae4
nashihulibad <alamin.ibad@gmail.com>
2022-06-09 11:22:13 +0700

    chore: adding middleware

--- BE/handler/middleware.go
+++ BE/handler/middleware.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) CheckUser(c *gin.Context) {
-	// TO DO Nashihul Ibad
+	ok := h.repo.IsLogin(c.Request.Header["Token"])
+	if !ok {
+		c.JSON(http.StatusForbidden,"Forbidden")
+		return
+	} else{
+		c.Next()
+	}
 }  
 
 func (h *Handler) CheckAdmin(c *gin.Context) {
-	// TO DO Nashihul Ibad
+	ok := h.repo.IsAdmin(c.Request.Header["Token"])
+	if !ok {
+		c.JSON(http.StatusForbidden,"Forbidden")
+		return
+	} else{
+		c.Next()
+	}
 }  
