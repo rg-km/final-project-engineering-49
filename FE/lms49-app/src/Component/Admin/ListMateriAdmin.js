08b5ee2c01ee9b52bbd8b1df720e7164f2609d17
Miqbal <miqbal.j00@gmail.com>
2022-06-14 13:59:07 +0700

    chore: sync

---
+++ FE/lms49-app/src/Component/Admin/ListMateriAdmin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ".Style/ListMateriAdmin.css";
+
+function ListMateriAdmin() {
+    return (
+        <div className="listMateriAdmin">
+            <h1>Ini List Materi Admin</h1>
+        </div>
+    )
+}
+
+export default ListMateriAdmin;
---
+++ FE/lms49-app/src/Component/Student/ListMateriStudent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ".Style/ListMateriStudent.css";
+
+function ListMateriStudent() {
+    return (
+        <div className="listMateriStudent">
+            <h1>Ini List Materi Student</h1>
+        </div>
+    )
+}
+
+export default ListMateriStudent;
