23e9119a2c7e1849b109ad98160c74a0009b009c
Safa Auliya <56856328+safaauliyaa@users.noreply.github.com>
2022-06-14 12:44:29 +0700

    Update materi.go
    handler materi

--- BE/handler/materi.go
+++ BE/handler/materi.go
@@ -1,38 +1,164 @@
 package handler
 
 import (
+	"belajar-golang/model/materi"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
-
-func (h *Handler) GetMateriByPage(c *gin.Context){
+func (h *Handler) GetMateriByPage(c *gin.Context) {
 	// TO DO Safa Auliya
 }
 
-func (h *Handler) GetMateriByFilter(c *gin.Context){
+func (h *Handler) GetMateriByFilter(c *gin.Context) {
 	// TO DO Safa Auliya
 }
 
-func (h *Handler) GetMateriByID(c *gin.Context){
+func (h *Handler) GetMateriByID(c *gin.Context) {
 	// TO DO Safa Auliya
+	MateriID, errConv := strconv.Atoi(c.Param("id"))
+	if errConv != nil {
+		c.JSON(http.StatusBadRequest, errConv.Error())
+		return
+	}
+
+	materi, err := h.repo.FindMateriByID(MateriID)
+	if err != nil {
+		c.JSON(http.StatusNotFound, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  200,
+		"message": "successfully",
+		"data":    materi,
+	})
 }
 
-func (h *Handler) GetAllMateri(c *gin.Context){
+func (h *Handler) GetAllMateri(c *gin.Context) {
 	// TO DO Safa Auliya
+	materis, count, err := h.repo.FindAllMateri()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	data := materi.MateriResponse{
+		Materi: materis,
+		Count:  count,
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Successfully",
+		"status":  200,
+		"data":    data,
+	})
 }
 
-func (h *Handler) CreateMateri(c *gin.Context){
+func (h *Handler) CreateMateri(c *gin.Context) {
 	// TO DO Safa Auliya
+	var materiRequest materi.MateriRequest
+	errReq := c.ShouldBindJSON(&materiRequest)
+	if errReq != nil {
+		c.JSON(http.StatusBadRequest,errReq.Error())
+		return
+	}
+
+	//upload file
+	var fileName string
+	if (materiRequest.Image != ""){
+		var base64File = materiRequest.Image
+		decodedByte, errDecode := base64.StdEncoding.DecodeString(base64File)
+		if errDecode != nil {
+			c.JSON(http.StatusBadRequest,errDecode.Error())
+			return
+		}
+		fileName = helper.GetFileName(0,materiRequest.Title)+".jpg"
+		ioutil.WriteFile("public/"+fileName, decodedByte, 0644)
+	} 
+
+	materi := materi.Materi{
+		LessonID: materiRequest.LessonID,
+		Title: materiRequest.Title,
+		Contain: materiRequest.Contain,
+		FileName: materiRequest.FileName,
+		Image : fileName,
+		Status : materiRequest.Status,
+		Creator: materiRequest.Creator,
+	}
+
+	materiCreated,err := h.repo.CreateMateri(materi)
+	if err != nil {
+		c.JSON(http.StatusBadRequest,err.Error())
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status" : 200,
+		"message" : "successfully",
+		"data" : materiCreated,
+	})
 }
 
-func (h *Handler) UpdateMateri(c *gin.Context){
+func (h *Handler) UpdateMateri(c *gin.Context) {
 	// TO DO Safa Auliya
+	var materiRequest materi.MateriRequest
+	errReq := c.ShouldBindJSON(&materiRequest)
+	if errReq != nil {
+		c.JSON(http.StatusBadRequest,errReq.Error())
+		return
+	}
+
+	if materiRequest.ID == 0 {
+		c.JSON(http.StatusBadRequest,fmt.Errorf("materi id null"))
+		return
+	}
+
+	materiFound,errFound := h.repo.FindMateriByID(materiRequest.ID)
+	if errFound != nil {
+		c.JSON(http.StatusBadRequest,fmt.Errorf("materi not found"))
+		return
+	}
+
+	os.Remove("public/"+materiFound.Image)
+
+	var fileName string
+	if (materiRequest.Image != ""){
+		var base64File = materiRequest.Image
+		decodedByte, errDecode := base64.StdEncoding.DecodeString(base64File)
+		if errDecode != nil {
+			c.JSON(http.StatusBadRequest,errDecode.Error())
+			return
+		}
+		fileName = helper.GetFileName(0,materiRequest.Title)+".jpg"
+		ioutil.WriteFile("public/"+fileName, decodedByte, 0644)
+	} 
+ 
+	materi := materi.Materi{
+		ID : materiRequest.ID,
+		LessonID: materiRequest.LessonID,
+		Title: materiRequest.Title,
+		FileName: materiRequest.FileName,
+		Image : fileName,
+		Contain: materiRequest.Contain,
+		Status : materiRequest.Status,
+		Creator: materiRequest.Creator,
+	}
+
+	materiUpdated,err := h.repo.UpdateMateri(materi)
+	if err != nil {
+		c.JSON(http.StatusBadRequest,err.Error())
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status" : 200,
+		"message" : "successfully",
+		"data" : materiUpdated,
+	})
+
 }
 
-func (h *Handler) DeleteMateri(c *gin.Context){
+func (h *Handler) DeleteMateri(c *gin.Context) {
 	// TO DO Safa Auliya
 }
-
-
-
-
