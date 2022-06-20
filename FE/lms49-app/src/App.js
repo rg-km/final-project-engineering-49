0c68229a062ba039632ff3b2e817569948752783
WidiyaNp <widiyanur231200@gmail.com>
2022-06-20 11:38:50 +0700

    ListMateriStudent

--- FE/lms49-app/src/App.js
+++ FE/lms49-app/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import Login from "./Component/Auth/Login";
 import Register from "./Component/Auth/Register";
 import Navbar from "./Component/Navbar/Navbar";
+import Student from "./Component/Student/ListMateriStudent"
 
 function App() {
     return (
-        <Navbar />
+        <Student/>
     )
 }
 
--- FE/lms49-app/src/Component/Student/ListMateriStudent.js
+++ FE/lms49-app/src/Component/Student/ListMateriStudent.js
@@ -5,6 +5,36 @@ function ListMateriStudent() {
     return (
         <div className="listMateriStudent">
             <h1>Ini List Materi Student</h1>
+            <div class="content">
+            <div class="cards">
+                <div class="card">
+                    <div class="box">
+                        <h1>Jumlah Course</h1>
+                        <h3>60</h3>
+                    </div>
+                </div>
+                </div>
+                </div>
+                <div class="content">
+            <div class="cards">
+                <div class="card">
+                    <div class="box">
+                        <h1>Jumlah Siswa</h1>
+                        <h3>15</h3>
+                    </div>
+                </div>
+                </div>
+                </div>
+                <div class="content">
+            <div class="cards">
+                <div class="card">
+                    <div class="box">
+                        <h1>Selamat Datang User 1!</h1>
+                        <h3>Students kaljuhb khhftd hgd</h3>
+                    </div>
+                </div>
+                </div>
+                </div>
         </div>
     )
 }
