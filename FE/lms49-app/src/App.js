4261ac642cfc9750056430f9dfa28f3a7c07daf6
BayuAdhiPrastyo <bayuadhiprastyo@gmail.com>
2022-06-21 20:14:31 +0700

    Sidebar tampilin images Update

--- FE/lms49-app/src/App.js
+++ FE/lms49-app/src/App.js
@@ -3,11 +3,24 @@ import Login from "./Component/Auth/Login";
 import Register from "./Component/Auth/Register";
 import CourseContent from "./Component/Dashboard/Dashboard-User-Course-Content";
 import Navbar from "./Component/Navbar/Navbar";
+import Sidebar from "./Component/Sidebar/Sidebar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Footer from "./Component/Footer/Footer";
+import ListMateriAdmin from "./Component/Admin/ListMateriAdmin";
 
-function App() {
+export default function App() {
     return (
-        <CourseContent/>
+    <div className="Sidebar">
+        <ListMateriAdmin />
+        {/* <Footer /> */}
+        <BrowserRouter>
+            <Sidebar />
+            <Switch>
+                <Route></Route>
+            </Switch> 
+        </BrowserRouter>
+    </div>
     )
 }
 
-export default App;
+
