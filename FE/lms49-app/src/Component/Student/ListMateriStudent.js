1022ca503e23227597b09541b85f826cfd3e9419
WidiyaNp <widiyanur231200@gmail.com>
2022-06-22 23:02:47 +0700

    FE/StudentComponent

--- FE/lms49-app/src/Component/Student/ListMateriStudent.js
+++ FE/lms49-app/src/Component/Student/ListMateriStudent.js
@@ -1,42 +1,100 @@
 import React from "react";
-import ".Style/ListMateriStudent.css";
+import "./Style/ListMateriStudent.css";
 
 function ListMateriStudent() {
     return (
-        <div className="listMateriStudent">
-            <h1>Ini List Materi Student</h1>
-            <div class="content">
-            <div class="cards">
-                <div class="card">
-                    <div class="box">
-                        <h1>Jumlah Course</h1>
-                        <h3>60</h3>
-                    </div>
-                </div>
-                </div>
-                </div>
-                <div class="content">
-            <div class="cards">
-                <div class="card">
-                    <div class="box">
-                        <h1>Jumlah Siswa</h1>
-                        <h3>15</h3>
-                    </div>
-                </div>
-                </div>
-                </div>
-                <div class="content">
-            <div class="cards">
-                <div class="card">
-                    <div class="box">
-                        <h1>Selamat Datang User 1!</h1>
-                        <h3>Students kaljuhb khhftd hgd</h3>
-                    </div>
-                </div>
-                </div>
-                </div>
+      <div className="container">
+        <div className="App">
+          <div className="gap">
+            <text className="text">Courses</text>
+          </div>
+          <div>
+            <button className="button">
+              <text className="textButton">Tambah Data</text>
+            </button>
+          </div>
         </div>
-    )
-}
-
+        <div className="main">
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1"></button>
+                <button className="button2"></button>
+              </div>
+            </div>
+          </div>
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1"></button>
+                <button className="button2"></button>
+              </div>
+            </div>
+          </div>
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1"></button>
+                <button className="button2"></button>
+              </div>
+            </div>
+          </div>
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1"></button>
+                <button className="button2"></button>
+              </div>
+            </div>
+          </div>
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1"></button>
+                <button className="button2"></button>
+              </div>
+            </div>
+          </div>
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1"></button>
+                <button className="button2"></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 export default ListMateriStudent;
