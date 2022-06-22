22f6342835372cc342d61dcbfab389f95e12de52
Miqbal <miqbal.j00@gmail.com>
2022-06-22 15:35:59 +0700

    chore: hit API on StudentBoard Page

--- FE/lms49-app/src/Component/Student/StudentDashboard.js
+++ FE/lms49-app/src/Component/Student/StudentDashboard.js
@@ -1,11 +1,50 @@
+import axios from "axios";
 import React from "react";
 import "./Style/StudentDashboard.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 import Footer from "../Footer/Footer";
 import Course from "../../Assets/course.png";
 
 function StudentDashboard() {
+
+    const [message, setMessage] = useState("");
+    const [countOfMateri, setCountOfMateri] = useState(0);
+    const [token, setToken] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate("/login");
+        }
+        setToken(token);
+
+        axios
+        .get("http://localhost:8080/user", {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {})
+        .catch((error) => {
+        navigate("/login");
+        });
+
+        axios
+          .get("http://localhost:8080/course/count", {
+            headers: { Authorization: `Bearer ${token}` },
+          })
+          .then((res) => {
+            const response = res.data;
+            setCountOfMateri(response.data);
+          })
+          .catch((error) => {
+            setMessage(error.message);
+          });
+
+    }, [])
+
     return (
       <div className="studentDashboard">
         <div className="NavbarComponent">
@@ -13,12 +52,13 @@ function StudentDashboard() {
         </div>
         <div className="BoxContainer">
           <div className="HeaderDashPage">
+            <h6>{message}</h6>
             <h1>Dashboard</h1>
           </div>
           <div className="BoxStatCount">
             <div className="textStat">
                 <h4>Jumlah Course</h4>
-                <h5>0</h5>
+                <h5>{countOfMateri}</h5>
             </div>
             <img className="logoCourse" src={Course}/>
           </div>
