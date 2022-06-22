feb48482254641d000533c2775abb7563fcfa23e
Miqbal <miqbal.j00@gmail.com>
2022-06-22 16:38:00 +0700

    chore: hit API on Detail Course Page

--- FE/lms49-app/src/Component/Dashboard/VideoJS.js
+++ FE/lms49-app/src/Component/Dashboard/VideoJS.js
@@ -1,12 +1,45 @@
+import axios from 'axios';
 import React from 'react';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ReactPlayer from 'react-player';
 
 
 function VideoJS() {
+
+    const [ message, setMessage ] = useState("");
+    const [ link, setLink ] = useState("");
+    const [ token, setToken ] = useState("");
+    const [ materiId, setMateriId ] = useState(0);
+    const params = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          navigate("/login");
+        }
+        setToken(token);
+        setMateriId(params.id);
+
+        axios
+        .get("http://localhost:8080/course" + params.id, {
+            headers: { Token: token}
+        })
+        .then((res) => {
+            const response = res.data
+            setLink(response.FileName);
+        })
+        .catch ((err) => {
+            setMessage(err.message);
+        })
+    }, []);
+
     return (
         <div className='videoo'>
             
-                <ReactPlayer url='https://www.youtube.com/watch?v=Ke90Tje7VS0' controls={true}/>
+                <ReactPlayer url={link} controls={true}/>
             
         </div>
     );
