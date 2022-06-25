32fd30e3dc9f711a889e1b89299941b7f78df0a5
Miqbal <miqbal.j00@gmail.com>
2022-06-25 15:47:06 +0700

    chore: update create materi page

--- FE/lms49-app/src/Component/Admin/CreateMateri.js
+++ FE/lms49-app/src/Component/Admin/CreateMateri.js
@@ -3,16 +3,58 @@ import Sidebar from '../Sidebar/Sidebar'
 import SidebarAdmin from './SidebarAdmin'
 import ListMateriAdmin from './ListMateriAdmin';
 import TambahData from './TambahData';
+import "./Style/CreateMateri.css";
 
 function CreateMateri() {
   return (
-    <div className='CreateMateri'>
-        {/* <Sidebar /> */}
-        {/* <SidebarAdmin /> */}
-        {/* <ListMateriAdmin /> */}
-        <TambahData />
+    <div className="CreateMateri">
+      <div className="CreateMateriForm">
+        <div className="containerCreateMateri">
+          <h2>ADD COURSE</h2>
+          <form>
+            <label>Nama Course</label>
+            <input
+              type="text"
+              name="nama_course"
+              placeholder="Input Judul..."
+              required
+            />
+
+            <label>Deskripsi</label>
+            <input
+              type="text"
+              name="deskripsi"
+              placeholder="Input Deskripsi..."
+              required
+            />
+
+            <label>Link Video</label>
+            <input
+              type="text"
+              name="link_vid"
+              placeholder="Input Link..."
+              required
+            />
+
+            <label>Creator</label>
+            <input
+              type="text"
+              name="creator_name"
+              placeholder="Input Crator..."
+              required
+            />
+
+            <div className="buttonCreate">
+              <button type="submit">Submit</button>
+            </div>
+            <div className="buttonCreate12">
+              <button type="submit">Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CreateMateri;
