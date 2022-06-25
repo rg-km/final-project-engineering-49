783c7145f8d82c5e7ec3a55e6b86e12dba770396
WidiyaNp <widiyanur231200@gmail.com>
2022-06-25 12:14:42 +0700

    FE/ComponentAdmin

--- FE/lms49-app/src/Component/Student/ListMateriStudent.js
+++ FE/lms49-app/src/Component/Student/ListMateriStudent.js
@@ -1,127 +1,98 @@
 import React from "react";
-import "./Style/ListMateriAdmin.css";
-import DeleteIcon from "../../Assets/del_butt.png";
-import EditIcon from "../../Assets/edit_butt.png";
+import Navbar from "../Navbar/Navbar"
+import "./Style/ListMateriStudent.css";
 
-function ListMateriAdmin() {
-  return (
-    <div className="container">
-      <div className="App">
-        <div className="gap">
-          <text className="text">Courses</text>
-        </div>
-        <div>
-          <button className="button">
-            <text className="textButton">Tambah Data</text>
-          </button>
-        </div>
-      </div>
-      <div className="main">
-        <div className="component">
-          <div className="image">Gambar</div>
-          <div className="footerBox">
-            <div className="title">
-              <text className="subTitle1">HTML</text>
-              <text className="subTitle2">Admin 2</text>
-            </div>
-            <div>
-              <button className="button1"> 
-              <img src={DeleteIcon} alt="delete" />
-               </button> 
-              <button className="button2">
-              <img src={EditIcon} alt="edit" />
-              </button> 
-            </div>
+const ListMateriStudent = () => {
+
+// function ListMateriStudent() {
+    return (
+      <div className="container">
+        <div className="App">
+          <div className="gap">
+            <text className="text">Courses</text>
+          </div>
+          <div>
+            <button className="button">
+              <text className="textButton">Tambah Data</text>
+            </button>
           </div>
         </div>
-        <div className="component">
-          <div className="image">Gambar</div>
-          <div className="footerBox">
-            <div className="title">
-              <text className="subTitle1">HTML</text>
-              <text className="subTitle2">Admin 2</text>
-            </div>
-            <div>
-              <button className="button1"> 
-              <img src={DeleteIcon} alt="delete" />
-               </button> 
-              <button className="button2">
-              <img src={EditIcon} alt="edit" />
-              </button> 
+        <div className="main">
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1">Read Now</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="component">
-          <div className="image">Gambar</div>
-          <div className="footerBox">
-            <div className="title">
-              <text className="subTitle1">HTML</text>
-              <text className="subTitle2">Admin 2</text>
-            </div>
-            <div>
-            <button className="button1"> 
-              <img src={DeleteIcon} alt="delete" />
-               </button> 
-              <button className="button2">
-              <img src={EditIcon} alt="edit" />
-              </button> 
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1">Read Now</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="component">
-          <div className="image">Gambar</div>
-          <div className="footerBox">
-            <div className="title">
-              <text className="subTitle1">HTML</text>
-              <text className="subTitle2">Admin 2</text>
-            </div>
-            <div>
-            <button className="button1"> 
-              <img src={DeleteIcon} alt="delete" />
-               </button> 
-              <button className="button2">
-              <img src={EditIcon} alt="edit" />
-              </button> 
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1">Read Now</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="component">
-          <div className="image">Gambar</div>
-          <div className="footerBox">
-            <div className="title">
-              <text className="subTitle1">HTML</text>
-              <text className="subTitle2">Admin 2</text>
-            </div>
-            <div>
-            <button className="button1"> 
-              <img src={DeleteIcon} alt="delete" />
-               </button> 
-              <button className="button2">
-              <img src={EditIcon} alt="edit" />
-              </button> 
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button2">Read Now</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="component">
-          <div className="image">Gambar</div>
-          <div className="footerBox">
-            <div className="title">
-              <text className="subTitle1">HTML</text>
-              <text className="subTitle2">Admin 2</text>
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button1">Read Now</button>
+              </div>
             </div>
-            <div>
-            <button className="button1"> 
-              <img src={DeleteIcon} alt="delete" />
-               </button> 
-              <button className="button2">
-              <img src={EditIcon} alt="edit" />
-              </button> 
+          </div>
+          <div className="component">
+            <div className="image">Gambar</div>
+            <div className="footerBox">
+              <div className="title">
+                <text className="subTitle1">HTML</text>
+                <text className="subTitle2">Admin 2</text>
+              </div>
+              <div>
+                <button className="button2">Read Now</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-export default ListMateriAdmin;
+    );
+  }
+  
+export default ListMateriStudent;
