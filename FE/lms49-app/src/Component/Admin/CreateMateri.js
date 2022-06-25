583f681b6bc58581013f530f00e7706da7930eb1
Miqbal <miqbal.j00@gmail.com>
2022-06-25 16:06:15 +0700

    chore: hit api on CreateMateri

--- FE/lms49-app/src/Component/Admin/CreateMateri.js
+++ FE/lms49-app/src/Component/Admin/CreateMateri.js
@@ -1,22 +1,76 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import React from 'react'
-import Sidebar from '../Sidebar/Sidebar'
-import SidebarAdmin from './SidebarAdmin'
-import ListMateriAdmin from './ListMateriAdmin';
-import TambahData from './TambahData';
 import "./Style/CreateMateri.css";
 
 function CreateMateri() {
+
+  const [ message, setMessage ] = useState("");
+  const [ token, setToken ] = useState("");
+  const [ title, setTitle ] = useState("");
+  const [ contain, setContain ] = useState("");
+  const [ link, setLink ] = useState("");
+  const [ creator, setCreator ] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+    setToken(token);
+
+    axios
+      .get("http://localhost:8080/user", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        const response = res.data;
+        if (response.data.Role != "admin") {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        navigate("/");
+      });
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      title: title,
+      contain: contain,
+      file_name: link,
+      creator: creator,
+    };
+    axios
+      .post("http://localhost:8080/course", data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setMessage("Success create course");
+      })
+      .catch((error) => {
+        console.log(error);
+        setMessage(error.message);
+      });
+  };
+
   return (
     <div className="CreateMateri">
       <div className="CreateMateriForm">
         <div className="containerCreateMateri">
+          <h6>{message}</h6>
           <h2>ADD COURSE</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>Nama Course</label>
             <input
               type="text"
               name="nama_course"
               placeholder="Input Judul..."
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
               required
             />
 
@@ -25,6 +79,8 @@ function CreateMateri() {
               type="text"
               name="deskripsi"
               placeholder="Input Deskripsi..."
+              value={contain}
+              onChange={(e) => setContain(e.target.value)}
               required
             />
 
@@ -33,6 +89,8 @@ function CreateMateri() {
               type="text"
               name="link_vid"
               placeholder="Input Link..."
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
               required
             />
 
@@ -41,14 +99,13 @@ function CreateMateri() {
               type="text"
               name="creator_name"
               placeholder="Input Crator..."
+              value={creator}
+              onChange={(e) => setCreator(e.target.value)}
               required
             />
 
             <div className="buttonCreate">
               <button type="submit">Submit</button>
-            </div>
-            <div className="buttonCreate12">
-              <button type="submit">Cancel</button>
             </div>
           </form>
         </div>
